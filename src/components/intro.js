3613520a209b937fc75893f465153b476058a39d
Alex Mireles <anchises1968@gmail.com>
2019-03-05 21:38:56 -0800

    styled services

--- src/components/intro.js
+++ src/components/intro.js
@@ -55,9 +55,9 @@ const Intro= () => (
             <div className="row">
               <div className="col">
                 <p className="greeting">Welcome!</p>
-                <p className="lead">Conscious Wellness’ mission is to help heal clients and empower them to attain the optimal health they want and deserve.</p>
+                <p className="lead">My mission is to help heal clients and empower them to attain the optimal health they want and deserve.</p>
                   <hr className="my-4"/>
-                <p>Conscious Wellness provides education and tools for you to achieve your optimal health. Humans are more than just a body part or a dis-ease. There is an interconnectedness of the body, mind and soul, and each are affected by the other. So our therapies work with the whole person.</p>
+                <p>I provides education and tools for you to achieve your optimal health. Humans are more than just a body part or a dis-ease. There is an interconnectedness of the body, mind and soul, and each are affected by the other. So our therapies work with the whole person.</p>
                 <p className="lead">
                   <button type="button" className="btn btn-outline-light btn-lg">Learn More</button>
                 </p>
